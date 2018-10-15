bcb8b2049ef2be5d01c6e6052a1ae5693ee34ec0
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-10-15 12:52:26 -0500

    added list of words to index.js; also added inquirer prompt for user letter guess

---
+++ index.js
@@ -0,0 +1,51 @@
+// index.js: The file containing the logic for the course of the game, which depends on Word.js and:
+
+// Randomly selects a word and uses the Word constructor to store it
+// Prompts the user for each guess and keeps track of the user's remaining guesses
+
+const inquirer = require("inquirer");
+
+const words = ["awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt", "dwarves", "fervid", "fishhook", "fjord",];
+
+
+
+
+
+
+
+
+
+
+
+
+"gazebo"
+
+"gypsy"
+
+function guess() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Guess a letter",
+                name: "letter"
+            },
+
+        ])
+        .then(function (response) {
+
+            let guess = response.letter;
+
+            let n = guess.length;
+
+            if (n > 1) {
+                console.log("please input a single letter only:");
+                guess();
+            } else {
+                console.log(guess);
+            }
+
+        });
+}
+
+guess();
