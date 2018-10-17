be9b1e8496dae91d9a8ae59f97957f8252fc73f0
Raghava Lakshminarayana <raglaks94@gmail.com>
2018-10-17 13:14:14 -0500

    added functions to check input

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ let wordString = null;
 
 let count = 0;
 
-let word = null;
-
 function randomWord() {
 
     const arrayLength = words.length;
@@ -18,8 +16,70 @@ function randomWord() {
 
     wordString = words[rando];
 
+}
+
+function guessInput() {
+
     console.log(wordString);
+
+    if (count <= 4) {
+
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "Guess a letter",
+                    name: "input"
+                },
+
+            ])
+            .then(response => {
+
+                let guess = response.input;
+
+                input = guess;
+
+                let n = guess.length;
+
+                if (n > 1) {
+
+                    console.log("please input one letter only");
+
+                    guessInput();
+
+                } else {
+
+                    count++;
+
+                    inputCheck();
+
+                }
+
+            });
+
+    } else {
+
+        console.log(`no more guesses bub`);
+
+        console.log(wordString);
+
+    }
+
+}
+
+function inputCheck() {
+    if (wordString.includes(input)) {
+        console.log(input);
+        console.log("correct");
+    } else {
+        console.log("wrong");
+        console.log("_");
+    }
+
+    guessInput();
 }
 
 randomWord();
+
+guessInput();
 
